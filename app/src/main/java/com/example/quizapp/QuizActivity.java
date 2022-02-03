02e88292772b5e9f8876e7a1a7c52bf9c2d66552
Iselin Thorsen Nilsen <iselin.thorsen.nilsen@gmail.com>
2022-02-03 19:14:23 +0100

    Comments on solution and refactoring

--- app/src/main/java/com/example/quizapp/QuizActivity.java
+++ app/src/main/java/com/example/quizapp/QuizActivity.java
@@ -9,6 +9,14 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_app);
+        setContentView(R.layout.activity_quiz);
     }
+
+    // Laste inn tilfeldig bilde fra DatabaseActivity
+
+    // Vise tilhørende tekst (riktig svaralternativ) sammen med to andre tekstalternativer som er feil
+
+    // Loope gjennom på denne måten i det uendelige
+
+    // Holde score
 }
